c7685e1879613dd4d95ff9fe5cbdf1077a9fc4f0
felipemat6767 <felipe_6767@hotmail.es>
2022-01-21 18:23:23 -0300

    Cambios21-01

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -14,64 +14,66 @@ import Foto9 from "../images/009.png"
 import Foto10 from "../images/010.png"
 import Foto11 from "../images/011.png"
 import Foto12 from "../images/012.png"
-const fotos = [Foto1, Foto2, Foto3, Foto4, Foto5, Foto6, Foto7, Foto8, Foto9,Foto10,Foto11, Foto12]
+const fotos = [Foto1, Foto2, Foto3, Foto4, Foto5, Foto6, Foto7, Foto8, Foto9, Foto10, Foto11, Foto12]
 
 
 
 
 const Home = () => {
-    
+
     useEffect(() => {
         actions.setInfo()
         actions.getName()
     }, [])
-   
+
+
+    const [input, setInput ] = useState('');
     const getInitialState = () => {
-        const value = "1";
+        const value = "0";
         return value;
-      };
-    
-      const [value, setValue] = useState(getInitialState);
-    
-      const handleChange = (e) => {
+    };
+    const [value, setValue] = useState(getInitialState);
+    const handleChange = (e) => {
         setValue(e.target.value);
-      };
-    
+    };
+
     const { store, actions } = useContext(Context)
+    
     const NombresA_Z = [...store.event]
-    
-    
-    
-    const carta = store.event.map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} pokname = {store.names[i]}/>) 
-    const cartarev = store.event.map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} pokname = {store.names[i]}/>)  
-    const Reverse = cartarev.reverse(function(num){ return num})
 
-    const cartaA_Z = NombresA_Z.sort((a,b)=> a.name>b.name ? 1: a.name<b.name ? -1: 0).map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]}/>)  
-    const cartaA_ZRev = NombresA_Z.sort((a,b)=> a.name>b.name ? 1: a.name<b.name ? -1: 0).map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]}/>)  
-    const A_ZRev = cartaA_ZRev.reverse(function(num){ return num})
 
+
+    const carta = store.event.map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} pokname={store.names[i]} />)
+    const cartarev = store.event.map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} pokname={store.names[i]} />)
+    const Reverse = cartarev.reverse(function (num) { return num })
+    const cartaA_Z = NombresA_Z.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0).map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} />)
+    const cartaA_ZRev = NombresA_Z.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0).map((pokemon, i) => <Pokemon data={pokemon} key={i} indice={i} image={fotos[i]} />)
+    const A_ZRev = cartaA_ZRev.reverse(function (num) { return num })
+    const Name = carta.filter(function (num) {if(num.props.data.name === input) {return num}})
+    console.log(input)
     return (
         <div className="container row d-flex m-3 align-content-center justify-content-center">
-            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" value ={value} onChange={handleChange}>
+            <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example" value={value} onChange={handleChange}>
                 <option>Busqueda</option>
-                <option value ="1" selected>Numero inferior</option>
+                <option value="1">Numero inferior</option>
                 <option value="2">Numero Superior</option>
                 <option value="3">A-Z</option>
                 <option value="4">Z-A</option>
             </select>
+            <input
+                type="text"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                placeholder="Ingresar Nombre de Pokemon"
+            />
             {console.log(value)}
-            {value === "0" ? carta:""}
-            {value === "1" ? carta:""}
-            {value === "2" ? Reverse:""}
-            {value === "3" ? cartaA_Z:""}
-            {value === "4" ? cartaA_ZRev:""}
-       
+            {value === "1" ? carta : ""}
+            {value === "2" ? Reverse : ""}
+            {value === "3" ? cartaA_Z : ""}
+            {value === "4" ? cartaA_ZRev : ""}
 
-            
-            
-          
-           
-              
+         
+            {Name}
         </div>
     )
 
