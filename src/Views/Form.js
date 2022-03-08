d36387a41823722dcf7d5eca7eb421570f203d67
felipemat6767 <felipe_6767@hotmail.es>
2022-03-08 03:22:28 -0300

    Cambios 08-03

--- src/Views/Form.js
+++ src/Views/Form.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 export const Form = () => {
     return (
-        <div>
-            <form>
-                <div>
-                    <div>
+        <div className='form-container'>
+            <form className='form'>
+                <div className='form-section'>
+                    <div className='form-element'>
                         <label>Nombre</label>
                         <input placeholder='Inserta tu nombre' name="Name" type="text" />
                     </div>
-                    <div>
+                    <div className='form-element'>
                         <label>Apellido</label>
                         <input placeholder='Inserta tu Apellido' name="LastName" type="text" />
                     </div>
                 </div>
                 
-                <div>
-                    <div>
+                <div className='form-section'>
+                    <div className='form-element'>
                         <label>Email</label>
                         <input placeholder='Escribe tu Correo' name="Email" type="email" />
                     </div>
-                    <div>
+                    <div className='form-element'>
                         <label>Celular</label>
                         <input placeholder='Escribe tu direccion' name="Phone" type="number" />
                     </div>
                 </div>
                 
-                <div>
+                <div className='form-section'>
                     <textarea />
                 </div>
             </form>
